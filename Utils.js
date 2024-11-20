4bdfad518a768612c2fd047edc15c270f7f094c7
weiqiangChow <48506218+weiqiangChow@users.noreply.github.com>
2024-11-20 17:29:35 +0800

    Update Utils.js

--- Utils.js
+++ Utils.js
@@ -11,39 +11,7 @@
  * @param {any} data
  * @return: type of data
  */
-export const dataType = data => {
-    const defaultTypeObj = {
-        '[object String]': 'String',
-        '[object Number]': 'Number',
-        '[object Object]': 'Object',
-        '[object Null]': 'Null',
-        '[object Undefined]': 'Undefined',
-        '[object Array]': 'Array',
-        '[object Set]': 'Set',
-        '[object Map]': 'Map',
-        '[object WeakMap]': 'WeakMap',
-        '[object Symbol]': 'Symbol',
-        '[object Function]': 'Function',
-        '[object Date]': 'Date',
-        '[object HTMLDocument]': 'HTMLDocument',
-        '[object Boolean]': 'Boolean',
-        '[object Arguments]': 'Arguments',
-        '[object Error]': 'Error',
-        '[object RegExp]': 'RegExp',
-        '[object ArrayBuffer]': 'ArrayBuffer',
-        '[object DataView]': 'DataView',
-        '[object Int8Array]': 'Int8Array',
-        '[object Int16Array]': 'Int16Array',
-        '[object Int32Array]': 'Int32Array',
-        '[object Uint8Array]': 'Uint8Array',
-        '[object Uint8ClampedArray]': 'Uint8ClampedArray',
-        '[object Float32Array]': 'Float32Array',
-        '[object Float64Array]': 'Float64Array',
-        '[object Uint16Array]': 'Uint16Array',
-        '[object Uint32Array]': 'Uint32Array',
-    }
-    return defaultTypeObj[Object.prototype.toString.call(data)]
-}
+export const dataType = data => Object.prototype.toString.call(data).slice(8,-1)
 
 /**
  *	color transform
